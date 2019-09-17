341da7619dca47249e4a65d92d8d15022be21c01
pyrdsy <pyry.koskinen@student.saimia.fi>
2019-09-17 15:24:41 +0300

    It works, trust me.

--- Example/Example/Program.cs
+++ Example/Example/Program.cs
@@ -6,7 +6,37 @@ namespace Example
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int summa = 0;
+            int i = 1;
+            Console.Write("Ohjelma laskee annettujen lukujen keskiarvon. " +
+                "Kuinka monta lukua syötät? ");
+            int lkm = int.Parse(Console.ReadLine());
+            
+            while(i<=lkm)
+            {
+                int x = int.Parse(Console.ReadLine());
+                summa = summa + x;
+                i = i + 1;
+            }
+            
+            if(lkm>0)
+            {
+                double ka = summa / lkm;
+                Console.WriteLine("Keskiarvo on: " + ka);
+            }
+            else
+         
+                Console.WriteLine("Lukuja ei syötetty!");
+            }
+
+            
+
+
+
+
+
+
+
         }
     }
 }
