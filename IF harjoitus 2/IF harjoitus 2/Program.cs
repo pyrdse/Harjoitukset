075a559405438f783b542a552159815ff6ab55a8
pyrdsy <pyry.koskinen@student.saimia.fi>
2019-09-18 14:38:32 +0300

    Kansion nimen muutos

---
+++ IF harjoitus 2/IF harjoitus 2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IF_harjoitus_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kuinka vanha olet? Oletko varusmies, opiskelija tai Mtkn jäsen: ");
+            Console.ReadKey();
+        }
+    }
+}
