9564c5ba495f6668bd3f15107ab8ab2191199921
pyrdsy <pyry.koskinen@student.saimia.fi>
2019-09-18 15:44:33 +0300

    FactorialExample DONE

--- Factorial harjoitus/Factorial harjoitus/Program.cs
+++ Factorial harjoitus/Factorial harjoitus/Program.cs
@@ -6,7 +6,34 @@ namespace Factorial_harjoitus
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = 0;                          // Define variables
+            int fact = 1;
+
+            Console.WriteLine("Kertoman laskenta");
+            Console.Write("Minkä luvun kertoma lasketaan?: ");
+
+
+            n = int.Parse(Console.ReadLine());  // Luetaan käyttäjän syöte ja pakotetaan se integer muotoon
+
+            if (n<0)                            // Ehto, jossa testataan käyttäjän syöttämä arvo. Ohjelman logiikka
+            {
+                Console.WriteLine("Virheellinen syöte");
+                Console.ReadLine();
+            }
+            else
+            {
+                while (n>0)                     // Silmukka jossa lasketaan kertoman arvo
+                {
+                    fact = fact * n;            // Lasketaan kertoma
+                    n = n - 1;                  // Vähennetään arvoa yhdellä
+                }
+                Console.WriteLine("Vastaus: " + fact);
+                Console.ReadLine();
+            }
+
+            
+
+           
         }
     }
 }
