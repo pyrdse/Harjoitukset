a98f95a0d36eb61c17443e008ef04dfbdffb654d
pyrdsy <pyry.koskinen@student.saimia.fi>
2019-10-29 18:29:28 +0200

    First task done

---
+++ Itsearviointi/Itsearviointi tehtävä 1/Itsearviointi tehtävä 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Itsearviointi_tehtävä_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write a word/sentence and I will print it five times: ");
+            string userInput = Console.ReadLine();
+            int n = 1;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(+n+(".")+(" ")+userInput);
+                n++;
+            }
+
+        }
+
+    
+
+
+    }
+
+}
