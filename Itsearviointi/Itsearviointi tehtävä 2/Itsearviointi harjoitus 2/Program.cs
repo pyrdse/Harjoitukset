8faa2c063d5a85dd7ef8fd7df312efeec1d89155
pyrdsy <pyry.koskinen@student.saimia.fi>
2019-10-29 18:54:25 +0200

    Task 2 Done

---
+++ Itsearviointi/Itsearviointi tehtävä 2/Itsearviointi harjoitus 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Itsearviointi_harjoitus_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write a word/sentence and I will print it as many time as it has letters: ");
+            string userInput = Console.ReadLine();
+
+            int numberOfLetters = userInput.Length;
+
+            for (int i = 1; i < numberOfLetters; i++)
+            {
+                Console.WriteLine(+i + (".") + (" ") + userInput);
+            }
+        }
+    }
+}
