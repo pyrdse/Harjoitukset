b309f22a42ddde90667e14bf4934468eaffcdaef
pyrdsy <pyry.koskinen@student.saimia.fi>
2019-10-30 11:04:38 +0200

    4.5 done

---
+++ Toistorakenne Harjoitukset/Toistorakenne 4.5/Toistorakenne 4.5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Toistorakenne_4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            Console.WriteLine("-----------");
+            Console.WriteLine("Lets gamble!");
+            Console.WriteLine("-----------\n");
+
+            for (int i = 1; i < 14; i++)
+            {
+                double result = rnd.NextDouble();
+
+                if (result <= 0.4)
+                {
+                    Console.WriteLine($"{i}. 1");                  
+                }
+                else if (result > 0.4 && result <= 0.6)
+                {
+                    Console.WriteLine($"{i}. x");                   
+                }
+                else
+                {
+                    Console.WriteLine($"{i}. 2");                  
+                }
+            }
+        }
+    }
+}
