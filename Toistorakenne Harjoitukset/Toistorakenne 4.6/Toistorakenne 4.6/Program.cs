5b406a0eca8c161b3359b354bf57be98296d42a3
pyrdsy <pyry.koskinen@student.saimia.fi>
2019-10-30 11:12:32 +0200

    Yes

---
+++ Toistorakenne Harjoitukset/Toistorakenne 4.6/Toistorakenne 4.6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Toistorakenne_4._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int six = 0;
+
+            for (int n = 0; n <= 1000; n++)
+            {
+                int result = rnd.Next(6);
+
+                Console.WriteLine($"{n}. {result + 1}");
+                if (result + 1 == 6)
+                {
+                    six++;
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine($"Numero kuusi arvottiin {six} kertaa.");
+            return;
+        }
+    }
+}
