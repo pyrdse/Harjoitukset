78d5a09fdbef8fabbec45e930d3425a966fe26c0
pyrdsy <pyry.koskinen@student.saimia.fi>
2019-10-30 12:15:02 +0200

    Itsearviointi 3 DONE

---
+++ Itsearviointi/Itsearviointi tehtävä 3/Itsearviointi tehtävä 3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Itsearviointi_tehtävä_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberToGuess = -1;
+            int userGuess = 0;
+            int sum = 0;
+
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Lets play a round of good old guessing game!");
+            Console.WriteLine("---------------------------------------------\n");
+            Console.WriteLine("Enter numbers between -10 and 10 until you enter the correct one: \n");
+
+            while (userGuess != numberToGuess)
+            {
+                
+                int.TryParse(Console.ReadLine(), out userGuess);
+
+                if (userGuess > numberToGuess)
+                {
+                    sum += userGuess;
+                }
+                else if (userGuess < numberToGuess)
+                {
+                    sum += userGuess;
+                }
+                else
+                {
+                    Console.WriteLine("\nThe sum of guessed numbers is {0}", sum);
+                }
+
+
+            }
+
+        }
+    }
+}
