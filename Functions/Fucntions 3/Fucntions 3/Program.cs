fa26f169c09487a2d1fc68b8257cc40df9818da2
pyrdsy <pyry.koskinen@student.saimia.fi>
2019-10-31 14:23:06 +0200

    Task 3 DONE

---
+++ Functions/Fucntions 3/Fucntions 3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Fucntions_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program will check if your input is valid.\n");
+
+            int retNumber = NumberFromRange(-20, 20);
+            Console.WriteLine($"\n{retNumber} is a valid number.");
+            Console.ReadKey();
+        }
+
+        static int NumberFromRange(int lowerBound, int upperBound)
+        {
+            do
+            {                   
+                Console.WriteLine($"Enter a number between {lowerBound} and {upperBound}");
+                string userInput = Console.ReadLine();
+
+                bool number = int.TryParse(userInput, out int userNumber);
+
+                if (number && userNumber >= lowerBound && userNumber <= upperBound)
+                {
+                    return userNumber;
+                }
+                else
+                {
+                    Console.WriteLine("\nInvalid input!");
+                }
+            } while (true);
+        }
+    }
+}
