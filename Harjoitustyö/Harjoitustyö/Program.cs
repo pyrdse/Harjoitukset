3a67ae63fc280bea2a138872a7b6233bdbd6f2dc
pyrdsy <pyry.koskinen@student.saimia.fi>
2020-01-27 15:43:42 +0200

    Finally started making the reference number task.

---
+++ Harjoitustyö/Harjoitustyö/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Harjoitustyö
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int menuChoice = 0;
+
+            do
+            {
+                menuChoice = openMenu();
+
+                if (menuChoice == 1)
+                {
+                    RefNumValidityCheck();
+                }
+                else if (menuChoice == 2)
+                {
+                    CreateNewRefNum();
+                }
+                else if (menuChoice == 3)
+                {
+                    CreateMultipleRefNum();
+                }
+            } while (menuChoice != 4);
+            Console.ReadKey();
+        }
+
+        private static int openMenu()
+        {
+            Console.WriteLine("This is a finnish reference number creator.\nMenu: press number for operation: \n");
+            Console.WriteLine("1. Check the validity of given reference number.");
+            Console.WriteLine("2. Create a new reference number.");
+            Console.WriteLine("3. Create multiple reference numbers.");
+            Console.WriteLine("4. Exit.");
+
+        }
+    }
+}
