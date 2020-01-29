37152f4a385857681193ddf15d2e8f7725a06154
pyrdsy <pyry.koskinen@student.saimia.fi>
2020-01-29 15:27:03 +0200

    Tasks 1 and 2 done.

---
+++ Merkkijono-tehtävät/StringTask 2/StringTask 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StringTask_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program replaces the character 'e' with '@'");
+            Console.WriteLine("Write a word or a sentence: ");
+            string str = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine(str.Replace('e', '@'));
+        }
+    }
+}
