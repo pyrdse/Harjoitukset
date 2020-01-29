af04ff1d43b3b789fe1000203be01b20990178de
pyrdsy <pyry.koskinen@student.saimia.fi>
2020-01-29 15:15:49 +0200

    Skipped from array task to string tasks.

---
+++ Merkkijono-tehtävät/StringTask 1/StringTask 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StringTask_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program shows the number of characters in user input");
+            Console.WriteLine("Write a word or a sentence: ");
+            string str = Console.ReadLine();
+            
+            Console.WriteLine();
+            Console.WriteLine($"Number of characters is {str.Length}.");            
+        }
+    }
+}
