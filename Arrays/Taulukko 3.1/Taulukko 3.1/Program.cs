33a22caa066f87509e15056303253a1881237af2
pyrdsy <pyry.koskinen@student.saimia.fi>
2020-01-29 14:49:52 +0200

    Started doing task 3.1

---
+++ Arrays/Taulukko 3.1/Taulukko 3.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Taulukko_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            Random rnd = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(20);
+            }
+            for (int y = 0; y < arr.Length; y++)
+            {
+                Console.WriteLine
+
+            }
+        }
+    }
+}
