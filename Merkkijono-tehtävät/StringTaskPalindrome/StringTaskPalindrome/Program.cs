a80aaed991bb5dcd25a229b4e25ebfe069025840
pyrdsy <pyry.koskinen@student.saimia.fi>
2020-01-29 17:11:07 +0200

    Palindrome task done.

---
+++ Merkkijono-tehtävät/StringTaskPalindrome/StringTaskPalindrome/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace StringTaskPalindrome
+{
+    class Program
+    {
+        
+        static void Main(string[] args)       
+        {
+            string userInput = string.Empty;
+            string reverse = string.Empty;
+
+            Console.WriteLine("This program checks whether a string is palindrome or not.");
+            Console.WriteLine("Enter a string");
+            userInput = Console.ReadLine();
+
+            userInput = userInput.Replace(" ", "");
+            reverse = reverse.Replace(" ", "");
+            
+
+            for(int i = userInput.Length-1; i >=0; i--)
+            {               
+                reverse += userInput[i].ToString();
+            }
+            if (reverse == userInput)
+            {
+                Console.WriteLine("\nString is palindrome.\nEntered string was {0} and reverse string is {1}", userInput, reverse);
+            }
+            else
+            {
+                Console.WriteLine("\nString is not palindrome. \nEntered string was {0} and reverse string is {1}", userInput, reverse);
+            }
+        }
+    }
+}
